ecf0400d07b90d8ec8c421d2dffd88a10af61485
Javy23 <191184@ids.upchiapas.edu.mx>
2022-12-04 19:48:58 -0600

    add cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 import express from "express";
 import employeesRoutes from './routes/employees.routes.js'
-
+import cors from 'cors'
 const app = express()
 
 app.use(express.json())
-
+app.use(cors())
 app.use('/api',employeesRoutes)
 
 app.use((req, res, next) => {
